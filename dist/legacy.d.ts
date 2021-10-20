133e042a7b82b030eec626e8668cc5b5273a18b6
Elliot Stoner <estoner@captechconsulting.com>
2021-10-19 22:05:44 -0500

    Add typescript .d.ts files

---
+++ dist/legacy.d.ts
@@ -0,0 +1,4 @@
+export var __esModule: boolean;
+export { f as generateXMLWorksheet };
+export function xljsxLite(e: any): any;
+declare function f(e: any): string;
---
+++ dist/module.d.ts
@@ -0,0 +1,2 @@
+export function generateXMLWorksheet(rows: any): string;
+export function xljsxLite(config: any): any;
